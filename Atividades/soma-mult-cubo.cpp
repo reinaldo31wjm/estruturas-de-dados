9660808632e956800c624c688ba2f67aeeab1e10
Rei <reinaldo31wjm@gmail.com>
2026-09-16 10:13:27 -0300

    Update soma-mult-cubo.cpp

--- Atividades/soma-mult-cubo.cpp
+++ Atividades/soma-mult-cubo.cpp
@@ -1,31 +1,30 @@
 #include <stdio.h>
 #include <locale.h>
 
-float fsoma(float p1, float p2) {
-    return (p1 + p2) + 10.0;
+int fsoma(int p1, int p2) {
+    return (p1 + p2) + 10;
 }
-float fmult(float res_soma, float p3) {
-    return (res_soma + p3) * 3.0;
+int fmult(int res_soma, int p3) {
+    return (res_soma + p3) * 3;
 }
-float fcubo(float res_mult) {
+int fcubo(int res_mult) {
     return res_mult * res_mult * res_mult;
 }
-
 int main() {
     setlocale(LC_ALL, "Portuguese");
-    float p1, p2, p3;
+    int p1, p2, p3;
 
-    printf("Digite o 1º número: ");
-    scanf("%f", &p1);
-    printf("Digite o 2º número: ");
-    scanf("%f", &p2);
-    printf("Digite o 3º número: ");
-    scanf("%f", &p3);
+    printf("Digite o 1Âº nÃºmero: ");
+    scanf("%d", &p1);
+    printf("Digite o 2Âº nÃºmero: ");
+    scanf("%d", &p2);
+    printf("Digite o 3Âº nÃºmero: ");
+    scanf("%d", &p3);
     
-    float res_s = fsoma(p1, p2);
-    float res_m = fmult(res_s, p3);
-    float res_c = fcubo(res_m);
-
-    printf("O resultado final é: %.2f\n", res_c);
+    int res_s = fsoma(p1, p2);
+    int res_m = fmult(res_s, p3);
+    int res_c = fcubo(res_m);
+    
+    printf("O resultado final Ã©: %d\n", res_c);
     return 0;
 }
